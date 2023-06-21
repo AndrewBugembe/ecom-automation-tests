ccc6e6a7a630159252c18223d39b5ac822fd73cf
Andrew QA <flashyandrew78@gmail.com>
2023-06-21 17:53:03 -0400

    more backend test framework code(connecting to database)

--- shoppingtests/tests/backend/createcustomer/test_create_customer.py
+++ shoppingtests/tests/backend/createcustomer/test_create_customer.py
@@ -2,6 +2,7 @@ import pytest
 import logging as logger
 from shoppingtests.src.utilities.genericUtilities import get_random_email_password
 from shoppingtests.src.helpers.apihelpers import CustomerHelper
+from shoppingtests.src.DAO.customerDAO import CustomerFromDb
 
 @pytest.mark.tb1
 def test_create_customer_email():
@@ -14,9 +15,17 @@ def test_create_customer_email():
     # return payload
 # make api call
     customer_object = CustomerHelper()
-    customer_api = customer_object.create_customer(email=email, passwordc=password)
+    customer_api = customer_object.create_customer(email=email, password=password)
+    # assert customer and email are created
+
+    assert customer_api["email"] == email, f'Email does not match. Email: {email}'
+    assert customer_api["first_name"] == '', f'First name should be empty'
+
+# verify customer created in database
+    customer_in_db = CustomerFromDb()
+    cuz = customer_in_db.get_customer_by_email(email)
+
     breakpoint()
 
 # assert status code
-# assert email is created
-# assert customer is creatred
+
